e0b6ec0572ff65a4a7d71ecded6afed01cd6a55a
24bit-xjkp <2283572185@qq.com>
2026-06-26 22:47:03 +0800

    build(xmake): 更新第三方库

--- script/package.lua
+++ script/package.lua
@@ -1,7 +1,7 @@
 package("clean_std_heads")
     set_homepage("https://github.com/YexuanXiao/convert-cpp-std-headers-to-std-module")
     set_urls("https://github.com/YexuanXiao/convert-cpp-std-headers-to-std-module/archive/refs/tags/$(version).zip")
-    add_versions("2026-06-26", "7ab33949aed983c8759a488b53bde686dc2d4f8d3512a6c57d45afa1737fd305")
+    add_versions("2026-06-26", "829ecc03ccb0dca7cdde504ddb931f76669d47d190443b16dfb8e8f5fb1da56a")
     set_kind("library", {headeronly = true})
 
     on_install(function (package)
