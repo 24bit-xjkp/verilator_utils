5b15137a054e7f7d75c41909fec4f920ba31cdc6
24bit-xjkp <2283572185@qq.com>
2026-06-26 15:55:29 +0800

    fix(xmake): 修复xmake构建顺序问题

--- rtl/xmake.lua
+++ rtl/xmake.lua
@@ -8,4 +8,10 @@ target("unit_test_rtl_edge_detector_verilator")
     add_files("edge_detector.sv")
     set_default(false)
     add_values("verilator.flags", table.join(verilator_options, {"--top", "edge_detector"}))
+    on_load(function (target)
+        local head_path = path.join(target:autogendir(), "rules", "verilator", target:name() .. ".h")
+        if (not os.isfile(head_path)) then
+            os.touch(head_path)
+        end
+    end)
 target_end()
