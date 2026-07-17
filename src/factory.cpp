733c97a02204b96f005d57b6d0e8da465e29c5d1
24bit-xjkp <2283572185@qq.com>
2026-07-17 23:35:12 +0800

    refactor(src): 删除无用属性
    1. 删除无用的gnu::visibility属性

--- src/factory.cpp
+++ src/factory.cpp
@@ -7,17 +7,14 @@ namespace verilator_utils::detail
     using namespace ::verilator_utils::verilator;
 
 #ifdef USE_VCD
-    [[gnu::visibility("default")]] ::std::unique_ptr<VerilatedVcdC> create_tracer_vcd()
-    { return ::std::make_unique<VerilatedVcdC>(); }
+    ::std::unique_ptr<VerilatedVcdC> create_tracer_vcd() { return ::std::make_unique<VerilatedVcdC>(); }
 #endif
 
 #ifdef USE_FST
-    [[gnu::visibility("default")]] ::std::unique_ptr<VerilatedFstC> create_tracer_fst()
-    { return ::std::make_unique<VerilatedFstC>(); }
+    ::std::unique_ptr<VerilatedFstC> create_tracer_fst() { return ::std::make_unique<VerilatedFstC>(); }
 #endif
 
 #ifdef USE_SAIF
-    [[gnu::visibility("default")]] ::std::unique_ptr<VerilatedSaifC> create_tracer_saif()
-    { return ::std::make_unique<VerilatedSaifC>(); }
+    ::std::unique_ptr<VerilatedSaifC> create_tracer_saif() { return ::std::make_unique<VerilatedSaifC>(); }
 #endif
 }  // namespace verilator_utils::detail
