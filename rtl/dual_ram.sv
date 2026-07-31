c1a11e4a563366f353c90f3ff0c619bb4d58e9e7
24bit-xjkp <2283572185@qq.com>
2026-08-01 01:40:35 +0800

    refactor(rtl): 移除dual_ram的interface接口

--- rtl/dual_ram.sv
+++ rtl/dual_ram.sv
@@ -1,45 +1,7 @@
-/* verilator lint_off DECLFILENAME */
 `ifndef RTL_DUAL_RAM
 `define RTL_DUAL_RAM
 
-interface dual_ram_port #(
-    parameter int data_width = 8,
-    parameter int addr_width = 3
-) (
-    input logic clk
-);
-    logic                    enable;
-    logic [addr_width - 1:0] addr;
-    logic [data_width - 1:0] data;
-
-    modport read(input clk, enable, addr, output data);
-    modport write(input clk, enable, addr, data);
-endinterface  // dual_ram_port
-
-module dual_ram (
-    dual_ram_port.read  read_port,
-    dual_ram_port.write write_port
-);
-    localparam int data_width = read_port.data_width;
-    localparam int addr_width = read_port.addr_width;
-
-    logic [data_width - 1:0] ram[1 << addr_width];
-
-    always_ff @(posedge read_port.clk) begin
-        if (read_port.enable) begin
-            read_port.data <= ram[read_port.addr];
-        end
-    end
-
-    always_ff @(posedge write_port.clk) begin
-        if (write_port.enable) begin
-            ram[write_port.addr] <= write_port.data;
-        end
-    end
-endmodule
-
-`ifndef NO_TOP_MODULE_WRAPPER
-module dual_ram_wrapper #(
+module dual_ram #(
     parameter int data_width = 8,
     parameter int addr_width = 3
 ) (
@@ -52,30 +14,18 @@ module dual_ram_wrapper #(
     input  logic [addr_width - 1 : 0] write_addr,
     input  logic [data_width - 1 : 0] write_data
 );
-    dual_ram_port #(
-        .data_width(data_width),
-        .addr_width(addr_width)
-    ) read_port (
-        .clk(read_clk)
-    );
-    dual_ram_port #(
-        .data_width(data_width),
-        .addr_width(addr_width)
-    ) write_port (
-        .clk(write_clk)
-    );
+    logic [data_width - 1:0] ram[1 << addr_width];
 
-    dual_ram u_dual_ram (
-        .read_port (read_port.read),
-        .write_port(write_port.write)
-    );
+    always_ff @(posedge read_clk) begin
+        if (read_enable) begin
+            read_data <= ram[read_addr];
+        end
+    end
 
-    assign read_port.enable  = read_enable;
-    assign read_port.addr    = read_addr;
-    assign read_data         = read_port.data;
-    assign write_port.enable = write_enable;
-    assign write_port.addr   = write_addr;
-    assign write_port.data   = write_data;
+    always_ff @(posedge write_clk) begin
+        if (write_enable) begin
+            ram[write_addr] <= write_data;
+        end
+    end
 endmodule
-`endif
 `endif
--- rtl/xmake.lua
+++ rtl/xmake.lua
@@ -1,20 +1,28 @@
-local verilator_options = {"-Wall", "--trace-fst", {"--x-assign", "unique"}, {"--x-initial", "unique"}, "--coverage"}
+local verilator_options = {
+    "-Wall",
+    "--trace-fst",
+    {"--x-assign", "unique"},
+    {"--x-initial", "unique"},
+    "--coverage",
+    "-DSIMULATION=1",
+}
 set_warnings("none")
 add_toolchains("@verilator")
 
 rtl_verilator_target = {
-    ["edge_detector"] = "edge_detector",
-    ["lfsr_m7"] = "lfsr_m7_wrapper",
-    ["counter"] = "counter",
-    ["sequence_detector"] = "sequence_detector",
-    ["dual_ram"] = "dual_ram_wrapper",
+    ["edge_detector"] = {},
+    ["lfsr_m7"] = {top = "lfsr_m7_wrapper"},
+    ["counter"] = {},
+    ["sequence_detector"] = {},
+    ["dual_ram"] = {},
 }
 
-for name, top_module in pairs(rtl_verilator_target) do
+for name, opt in pairs(rtl_verilator_target) do
     target(format("unit_test_rtl_%s_verilator", name))
         add_rules("verilator.shared")
-        add_files(name.."*.sv")
+        add_files(table.join(opt["deps"] or {}, {name..".sv"}))
         set_default(false)
+        local top_module = opt["top"] or name
         add_values("verilator.flags", table.join(verilator_options, {"--top", top_module}))
         set_policy("build.fence", true)
         on_load(function (target)
