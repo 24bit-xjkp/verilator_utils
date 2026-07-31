0a6478db710d2206858846f1a0c736c1a2f4eaf9
24bit-xjkp <2283572185@qq.com>
2026-08-01 02:25:23 +0800

    refactor(src): 重排序导入声明

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,8 +1,8 @@
 export module verilator_utils;
-export import :utils;
-export import :wrapper;
-export import :scheduler;
 export import :context;
-export import :task;
-export import :verilator;
 export import :internal;
+export import :scheduler;
+export import :task;
+export import :utils;
+export import :verilator;
+export import :wrapper;
