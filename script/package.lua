d10d3ae8eeaa17a0389a5f411deb73e1e4c246ee
24bit-xjkp <2283572185@qq.com>
2026-08-01 17:54:06 +0800

    build(xmake): 更新依赖包

--- script/package.lua
+++ script/package.lua
@@ -1,7 +1,8 @@
 package("clean_std_heads")
     set_homepage("https://github.com/YexuanXiao/convert-cpp-std-headers-to-std-module")
-    set_urls("https://github.com/YexuanXiao/convert-cpp-std-headers-to-std-module/archive/refs/tags/$(version).zip")
-    add_versions("2026-06-26", "829ecc03ccb0dca7cdde504ddb931f76669d47d190443b16dfb8e8f5fb1da56a")
+    set_urls("https://github.com/YexuanXiao/convert-cpp-std-headers-to-std-module.git")
+    add_versions("1.0", "73b5212319bc9dab6a0bdc12afed5036b153bb7e")
+    add_versions("1.1", "00ae6cc3184d9e55cae67fc9ff305a235b968e6e")
     set_kind("library", {headeronly = true})
 
     on_install(function (package)
--- src/doctest_impl.cpp
+++ src/doctest_impl.cpp
@@ -1,6 +1,5 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 // NOLINTNEXTLINE(bugprone-reserved-identifier)
-#define __EXCEPTION__
 #include "doctest_fwd.hpp"
 import std;
 import std.compat;
